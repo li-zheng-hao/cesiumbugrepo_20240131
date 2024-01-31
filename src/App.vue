4570a4501b4e9bf5e170ea1d5e3fb31ff0537fc2
LiZhengHao <1263212577@qq.com>
2024-01-31 21:26:15 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import NButton from './components/NButton.vue';
+const click=()=>alert('click');
+const viewer=ref(null)
+const tags=ref()
+function clickMap(params:any) {
+  console.log(params);
+  
+}
 </script>
 
 <template>
   <div class="root"> 
-    <vc-viewer>
+    <vc-viewer @leftClick="clickMap" :ref="viewer">
     <!-- 注记层 -->
     <vc-layer-imagery  :sort-order="20">
       <vc-imagery-provider-tianditu map-style="cva_c"   token="436ce7e50d27eede2f2929307e6b33c0"></vc-imagery-provider-tianditu>
     </vc-layer-imagery>
     <vc-layer-imagery  :sort-order="10">
       <vc-imagery-provider-tianditu   token="436ce7e50d27eede2f2929307e6b33c0" ref="provider"></vc-imagery-provider-tianditu>
-      
     </vc-layer-imagery>
+    <vc-overlay-html :position="{ lng: 104.04, lat: 30.40 }" @click="click" style="display: flex;flex-direction: column;" :ref="tags">
+      <NButton></NButton>
+      </vc-overlay-html>
   </vc-viewer>
   </div>
 </template>
---
+++ src/components/NButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="button-container" >
+        <div class="title">这是标题</div>
+        <div class="container">
+            这是内容
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.button-container{
+    width: 150px;
+    height: 100px;
+    background-color: aqua;
+    display: flex;
+    flex-direction: column;
+}
+.title{
+    background-color: blue;
+}
+</style>
