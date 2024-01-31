7f8434570308521ec6efdbadd7afb86df9a601c6
LiZhengHao <1263212577@qq.com>
2024-01-31 20:26:43 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="root"> 
+    <vc-viewer>
+    <!-- 注记层 -->
+    <vc-layer-imagery  :sort-order="20">
+      <vc-imagery-provider-tianditu map-style="cva_c"  :minimumLevel="15" token="436ce7e50d27eede2f2929307e6b33c0"></vc-imagery-provider-tianditu>
+    </vc-layer-imagery>
+    <vc-layer-imagery  :sort-order="10">
+      <vc-imagery-provider-tianditu  :minimumLevel="15"  token="436ce7e50d27eede2f2929307e6b33c0" ref="provider"></vc-imagery-provider-tianditu>
+    </vc-layer-imagery>
+  </vc-viewer>
+  </div>
+</template>
+
+<style scoped>
+.root{
+  width: 100vw;
+  height: 100vh;
+
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import VueCesium from 'vue-cesium'
+import App from './App.vue'
+import 'vue-cesium/dist/index.css'
+const app=createApp(App)
+app.use(VueCesium)
+app.mount('#app')
