5bc1ceeea477fa17d8003cfb142000201de2087b
LiZhengHao <1263212577@qq.com>
2024-01-31 20:53:54 +0800

    离线版本

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,11 @@
     <vc-viewer>
     <!-- 注记层 -->
     <vc-layer-imagery  :sort-order="20">
-      <vc-imagery-provider-tianditu map-style="cva_c"  :minimumLevel="15" token="436ce7e50d27eede2f2929307e6b33c0"></vc-imagery-provider-tianditu>
+      <vc-imagery-provider-tianditu map-style="cva_c"   token="436ce7e50d27eede2f2929307e6b33c0"></vc-imagery-provider-tianditu>
     </vc-layer-imagery>
     <vc-layer-imagery  :sort-order="10">
-      <vc-imagery-provider-tianditu  :minimumLevel="15"  token="436ce7e50d27eede2f2929307e6b33c0" ref="provider"></vc-imagery-provider-tianditu>
+      <vc-imagery-provider-tianditu   token="436ce7e50d27eede2f2929307e6b33c0" ref="provider"></vc-imagery-provider-tianditu>
+      
     </vc-layer-imagery>
   </vc-viewer>
   </div>
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ import VueCesium from 'vue-cesium'
 import App from './App.vue'
 import 'vue-cesium/dist/index.css'
 const app=createApp(App)
-app.use(VueCesium)
+app.use(VueCesium,{
+    cesiumPath:"/Cesium@1.113.0/Cesium.js"
+})
 app.mount('#app')
